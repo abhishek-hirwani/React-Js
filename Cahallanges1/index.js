ba7e37fbe757321c7270cffb0ad327da8849d652
Abhishek Hirwani <abhihirwani56@gmai.com>
2022-03-10 11:30:41 +0530

    challange added

---
+++ Cahallanges1/index.js
@@ -0,0 +1,15 @@
+const page=(
+ <div>
+  <img src="./logo.png" alt="react-logo" width="40px"/>
+  <h1>Fun facts about React</h1>
+  <ul>
+   <li>was first released in 2013</li>
+   <li>was originaly created by the jordan walke</li>
+   <li>has well over 100k stats on github</li>
+   <li>Powers thousand of enterprises apps, inlcuding mobile apps</li>
+
+  </ul>
+ </div>
+)
+
+ReactDOM.render(page, document.getElementById("admin"))
