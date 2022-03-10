0d09171cadffbe265f7e29367638e121ce40a24e
Abhishek Hirwani <abhihirwani56@gmai.com>
2022-03-10 11:18:40 +0530

    memegenerator added

---
+++ MemeGenerator/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Head(){
+ return(
+  
+      <header className="header">
+       <img src="..images/memelogo.png " className="header--img"/>
+       <h2 className="header--title">MemeGenerator</h2>
+       <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+ )
+}
