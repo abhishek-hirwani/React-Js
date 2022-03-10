4a7ad7dc1822fc7cf1a3b98f3c98c502f37e8045
Abhishek Hirwani <abhihirwani56@gmai.com>
2022-03-10 11:29:23 +0530

    challange added

---
+++ Challanges/index.js
@@ -0,0 +1,15 @@
+
+const page=(
+   <div>
+    <h1>My Awesome website in react </h1>
+    <h3>Reason i love react</h3>
+    <ol>
+     <li>it's composable</li>
+     <li>it's declarative</li>
+     <li>it's a hireable skill</li>
+     <li>it's sctively maintianed by skilled people</li>
+    </ol>
+    </div>
+)
+
+ReactDOM.render(page,document.getElementById("admin"))
